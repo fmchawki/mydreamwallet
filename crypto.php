39b20fe2b7b7ca2933d3163e34dd61ea7e3a984e
ThomasMrchl <marchalthomas88@gmail.com>
2022-04-26 15:57:55 +0200

    add quantity crypto data on the table

--- crypto.php
+++ crypto.php
@@ -8,12 +8,44 @@
 </head>
 <body>
 
+<!-- input tag -->
+<input id="searchbar" onkeyup="search_crypto()" type="text"
+        name="search" placeholder="Trouver une crypto..">
+      
+    <!-- ordered list -->
+    <ol id='list'>
+        <li class="cryptobar">BITCOIN</li>
+        <li class="cryptobar">ETHEREUM</li>
+        <li class="cryptobar">BINANCE COIN</li>
+        <li class="cryptobar">SOLANA</li>
+        <li class="cryptobar">CHAIN LINK</li>
+        <li class="cryptobar">POLSKA DOT</li>
+        <li class="cryptobar">CARDANO</li>
+        <li class="cryptobar">XRP</li>
+        <li class="cryptobar">AVAX</li>
+        <li class="cryptobar">LUNA</li>
+    </ol>
+      
+    <!-- linking javascript -->
+    <script src="js/search.js"></script>
+
 <?php 
 
 $valeur  = "new_utilisateur";
 $valeur2 = "mail";
 $valeur3 = "nationalité";
 $valeur4 = "id";
+$valeurwallet = 0;
+$qbtc = 0;
+$qeth = 0;
+$qbnb = 0;
+$qsol = 0; 
+$qlink = 0; 
+$qdot = 0; 
+$qada = 0; 
+$qxrp = 0; 
+$qavax = 0; 
+$qluna = 0; 
 $nu="";
 if (isset ($_SESSION['pseudo'])){
     $nu = "1";
@@ -40,38 +72,27 @@ $db_name     = 'mydreamwallet_base';
 $db_host     = 'mysql-mydreamwallet.alwaysdata.net';
 $db = mysqli_connect($db_host, $db_username, $db_password,$db_name)
     or die('could not connect to database');
-$requete = "SELECT * FROM mywallet JOIN mywallet.id_user ON User.id WHERE User.id='".$output4."'; ";
+$requete = "SELECT * FROM mywallet JOIN User ON User.id = mywallet.id_user WHERE User.id='".$output4."'; ";
 $exec_requete = mysqli_query($db,$requete);
 $count = mysqli_num_rows($exec_requete);
 for ($j = 0; $j < $count; $j++) {
   $ligne = mysqli_fetch_array($exec_requete);
   $valeurwallet = $ligne["vwallet"];
-echo $valeurwallet;
+  $qbtc = $ligne["q_btc"];
+  $qeth = $ligne["q_eth"];
+  $qbnb = $ligne["q_bnb"];
+  $qsol = $ligne["q_sol"];
+  $qlink = $ligne["q_link"];
+  $qdot = $ligne["q_dot"];
+  $qada = $ligne["q_ada"];
+  $qxrp = $ligne["q_xrp"];
+  $qavax = $ligne["q_avax"];
+  $qluna = $ligne["q_luna"];
 }
 }
 
 ?>
 
-<!-- input tag -->
-<input id="searchbar" onkeyup="search_crypto()" type="text"
-        name="search" placeholder="Trouver une crypto..">
-      
-    <!-- ordered list -->
-    <ol id='list'>
-        <li class="cryptobar">BITCOIN</li>
-        <li class="cryptobar">ETHEREUM</li>
-        <li class="cryptobar">BINANCE COIN</li>
-        <li class="cryptobar">SOLANA</li>
-        <li class="cryptobar">CHAIN LINK</li>
-        <li class="cryptobar">POLSKA DOT</li>
-        <li class="cryptobar">CARDANO</li>
-        <li class="cryptobar">XRP</li>
-        <li class="cryptobar">AVAX</li>
-        <li class="cryptobar">LUNA</li>
-    </ol>
-      
-    <!-- linking javascript -->
-    <script src="js/search.js"></script>
 
 <div class="container">
   <table class="responsive-table">
@@ -88,10 +109,19 @@ echo $valeurwallet;
     </thead>
     <tbody>
       <tr>
+        <th scope="row">CASH</th>
+        <td data-title="Released">//</td>
+        <td data-title="Studio">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $valeurwallet; ?></td>
+        <td data-title="Domestic Gross" data-type="currency"> $ </td>
+        <td data-title="International Gross" data-type="currency">⬇</td>
+        <td data-title="Budget" data-type="currency">⬇</td>
+      </tr>
+      <tr>
         <th scope="row">BITCOIN</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qbtc; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-btc">BTC</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -100,7 +130,7 @@ echo $valeurwallet;
         <th scope="row">ETHEREUM</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qeth; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-eth">ETH</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -109,7 +139,7 @@ echo $valeurwallet;
         <th scope="row">BINANCE COIN</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qbnb; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-bnbbtc">BNBC</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -118,7 +148,7 @@ echo $valeurwallet;
         <th scope="row">SOLANA</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qsol; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-sol">SOL</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -127,7 +157,7 @@ echo $valeurwallet;
         <th scope="row">CHAIN LINK</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qlink; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-link">LINK</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -136,7 +166,7 @@ echo $valeurwallet;
         <th scope="row">POLKA DOT</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qdot; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-dot">DOT</td></td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -145,7 +175,7 @@ echo $valeurwallet;
         <th scope="row">CARDANO</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qada; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-wbt">ADA</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -154,7 +184,7 @@ echo $valeurwallet;
         <th scope="row">XRP</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qxrp; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-xrp">XRP</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -163,7 +193,7 @@ echo $valeurwallet;
         <th scope="row">AVAX</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qavax; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-ava">AVAX</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -172,7 +202,7 @@ echo $valeurwallet;
         <th scope="row">LUNA</th>
         <td data-title="Released">//</td>
         <td data-title="Studio">//</td>
-        <td data-title="Worldwide Gross" data-type="currency">//</td>
+        <td data-title="Worldwide Gross" data-type="currency"><?php echo $qluna; ?></td>
         <td data-title="Domestic Gross" data-type="currency" id="stock-price-lun">LUNA</td>
         <td data-title="International Gross" data-type="currency"><div class="button-green">Buy</div></td>
         <td data-title="Budget" data-type="currency"><div class="button-red">Sell</div></td>
@@ -180,14 +210,6 @@ echo $valeurwallet;
     </tbody>
   </table>
 </div>
-<div id="askcrypto">
-    <?php
-    if (isset ($_REQUEST['carotte'])){
-      $vote = $_REQUEST['carotte'];
-      echo $vote;
-    }
-    ?> 
-    </div>
-<script src='js/eth.js'></script>
+
 </body>
 </html>
